ec621aa572b09bd520bf8d820f4d7b758f4b1042
Fatos Morina <fatosi.morina@gmail.com>
2017-04-23 11:21:16 +0200

    Add Features module

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -7,14 +7,17 @@ RSpec.describe 'User signing in' do
   end
   
   scenario 'successfully' do
-    visit new_user_session_path
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    click_on 'Sign in'
+    sign_in @user
+    expect(page).to have_content @user.email
   end
 
   scenario 'unsuccessfully' do
-
+    visit new_user_session_path
+    fill_in 'Email', with: 'not valid email'
+    fill_in 'Password', with: 'blank_password'
+    click_on 'Sign in'
+    
+    expect(page).not_to have_content @user.email
   end
   
 end
