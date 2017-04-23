0bceb2fcc4e453dde5e1e7b5e766cb0924067813
Fatos Morina <fatosi.morina@gmail.com>
2017-04-23 14:02:09 +0200

    Add username to sign up forM

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature 'User signing up' do
 
     scenario 'successfully' do
       visit new_user_registration_path
+      fill_in 'Username', with: 'Username'
       fill_in 'Email', with: 'email@email.com'
       fill_in 'Password', with: 'qwerty123'
       fill_in 'Password confirmation', with: 'qwerty123'
