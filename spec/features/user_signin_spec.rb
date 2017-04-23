a07b71acd9bed715d0619fd85134159bc877fb29
Fatos Morina <fatosi.morina@gmail.com>
2017-04-23 11:09:42 +0200

    Add user signin spec

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'User signing in' do
   
+  background do
+    @user = User.create!(email: 'email@email.com', password: 'password', password_confirmation: 'password')
+  end
+  
   scenario 'successfully' do
-    
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_on 'Sign in'
   end
 
   scenario 'unsuccessfully' do
