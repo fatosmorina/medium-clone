abe6a215adaac53b645a09792edda86ebaec7742
Fatos Morina <fatosi.morina@gmail.com>
2017-04-24 20:16:33 +0200

    Declare model associations between Tag, Tagging, Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+  has_many :taggins, dependent: :destroy
+  has_many :tags, through: :taggins
+
   validates :title, :body, presence: true
-  
+  	
   belongs_to :user
 
   delegate :username, to: :user
