1f5418cc844c22efcc21136f9d983da052001a6f
Fatos Morina <fatosi.morina@gmail.com>
2017-04-25 11:18:28 +0200

    Implement follow/unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,22 @@
 class RelationshipsController < ApplicationController
 
 	before_action :authenticate_user!
+	before_action :find_user
 
 	def create
-	  user = User.find(params[:user_id])
-	  current_user.follow(user)
-	  redirect_to user
+	  current_user.follow(@user)
+	  redirect_to @user
 	end
 
 	def destroy
+	  current_user.unfollow(@user)	
+	  redirect_to @user
 	end		
 
+
+  private 
+
+  def find_user
+  	@user = User.find(params[:user_id])
+  end
 end
