72a8d9f13fb49c667037d95720c9f87818037749
Fatos Morina <fatosi.morina@gmail.com>
2017-04-25 09:13:32 +0200

    Add relationships between relations and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
   				foreign_key: 'follower_id', dependent: :destroy
   
-  has_many :following, through: :active_relationships				
+  has_many :followings, through: :active_relationships				
 
 end
 
