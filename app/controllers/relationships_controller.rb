9065e8ae678c0cb94d6b46984a8f22530205564a
Fatos Morina <fatosi.morina@gmail.com>
2017-04-25 11:09:56 +0200

    Implement follow user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,6 +3,9 @@ class RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
+	  user = User.find(params[:user_id])
+	  current_user.follow(user)
+	  redirect_to user
 	end
 
 	def destroy
