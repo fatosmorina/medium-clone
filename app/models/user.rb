35565f94f8df353c28be09ccc649db45efddaf56
Fatos Morina <fatosi.morina@gmail.com>
2017-04-25 09:51:35 +0200

    Add do/end at user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
 
 
   has_many :passive_relationships, class_name: 'Relationship',
-  				foreign_key: 'followed_id', depdendent: :destroy
+  				foreign_key: 'followed_id', dependent: :destroy
 
 
-  has_many :followers, :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :active_relationships, class_name: 'Relationship',
   				foreign_key: 'follower_id', dependent: :destroy
@@ -20,16 +20,17 @@ class User < ApplicationRecord
 
 
   def follow(other_user)
-  	active_relationships.create(followed_id: other_user.id)
-  end
-  
-  def unfollow(other_user)
-  	active_relationships.find_by(followed_id: other_user.id).destroy
+    return false if self.id == other_user.id
+    active_relationships.create(followed_id: other_user.id)
   end
 
-  def folllowing?(other_user)
-  	following_ids.include?(other_user.id)
-  end	 	
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following_ids.include?(other_user.id)
+  end 	
 
   validates :username, presence: true
   mount_uploader :avatar, AvatarUploader
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User 
+RSpec.describe User do
 	
 
 	describe 'following and unfollowing other users' do
 	  let(:filan) { create(:user, username: 'Filan Fisteku')}
 	  let(:solo){ create(:user, username: 'Ben Doe') }	
 
-	  it 'adds relationships between two users' do
+	  it 'follows and unfollows a user' do
 	  	filan.follow(solo)
 	  	expect(filan.following?(solo)).to be_truthy
-
+	  	
 	  	filan.unfollow(solo)
 	  	expect(filan.following?(solo)).to be_falsy
-	end	
+	  end
+	 
+	end
+
 end
